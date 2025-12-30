961aac8cced4204df1f0d9c0d6862bada4ba5427
ahmedmohsen5 <a.khalefa555@gmail.com>
2025-12-30 04:31:36 +0200

    fix# s3 bucket

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    backend = "tfstate-as-voting-app-dev"
+    bucket = "tfstate-as-voting-devops-001"
     key = "dev/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "tf-locks-voting-devops"
